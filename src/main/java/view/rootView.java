387926285e1072d353ac83e6c021698e3c94f8bc
JohnnyJapes <lhanrahan2@gmail.com>
2023-09-27 18:35:22 -0400

    add view folder + root window

---
+++ src/main/java/view/rootView.java
@@ -0,0 +1,31 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import main.HelloApplication;
+
+import java.io.IOException;
+
+public class rootView extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        BorderPane rootLayout = new BorderPane();
+
+        VBox listPane = new VBox(5);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/view/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello World!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
