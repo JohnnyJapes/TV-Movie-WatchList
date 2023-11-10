c1864add08035723b868bd44d6764b175818e8fc
JohnnyJapes <lhanrahan2@gmail.com>
2023-11-10 15:48:39 -0500

    creatRow method for ListEntry

--- src/main/java/model/ListEntry.java
+++ src/main/java/model/ListEntry.java
@@ -1,6 +1,9 @@
 package model;
 
 import model.content.ContentBase;
+import model.content.Movie;
+
+import java.sql.*;
 
 public class ListEntry {
 
@@ -47,10 +50,67 @@ public class ListEntry {
     public void setListRank(int listRank) {
         this.listRank = listRank;
     }
+
+    @Override
+    public String toString() {
+        return listRank + ". " + entry.getTitle();
+
+    }
+
     //CRUD operations
     public void createRow(){
 
+        Connection connection = null;
+        try
+        {
+            // create a database connection
+            connection = DriverManager.getConnection("jdbc:sqlite:local.db");
+            PreparedStatement statement = connection.prepareStatement("insert into listentries(list_id, content_id, rank) values(?,?,?)");
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+            statement.setInt(1,listID);
+            statement.setInt(2,getEntry().getID());
+            statement.setInt(3,getListRank());
+
+
+
+
+            statement.executeUpdate();
+            ResultSet rs = connection.createStatement().executeQuery("select * from listentries");
+            while(rs.next())
+            {
+                Movie temp = new Movie();
+                // read the result set
+                System.out.println("list_id = " + rs.getInt("list_id"));
+                System.out.println("content_id = " + rs.getInt("content_id"));
+                temp.readRow(rs.getInt("content_id"));
+                System.out.println("Entry Title = " + temp.getTitle());
+                System.out.println("Rank = " + rs.getInt("rank"));
+
+            }
+        }
+        catch(SQLException e)
+        {
+            // if the error message is "out of memory",
+            // it probably means no database file is found
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                if(connection != null)
+                    connection.close();
+            }
+            catch(SQLException e)
+            {
+                // connection close failed.
+                System.err.println(e.getMessage());
+            }
+        }
     }
+
     public void readRow(int id){
 
     }
@@ -63,9 +123,6 @@ public class ListEntry {
 
     }
 
-    @Override
-    public String toString() {
-        return listRank + ". " + entry.getTitle();
 
-    }
+
 }
